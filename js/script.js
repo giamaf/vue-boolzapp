992636fe9a48bb5e9aa663eece535df641385b30
giamaf <gianluca.maffucci@hotmail.com>
2024-01-26 12:45:37 +0100

    added dinamic messages view

--- js/script.js
+++ js/script.js
@@ -17,6 +17,11 @@ const myApp = createApp({
         // # Recupero il contatto cliccato dall'utente perchè è qualcosa che cambia in tempo reale. Se ho il contatto, posso accedere quindi a foto, nome ecc...
         activeContact() {
             return this.contacts.find(contact => contact.id === this.currentId)
+        },
+
+        // # Recupero la chat dei vari contatti 
+        contactChat() {
+            return this.activeContact.messages
         }
     },
 
@@ -25,7 +30,7 @@ const myApp = createApp({
         // # Il parametro è l'id dell'utente cliccato '(contact.id)'
         setCurrentId(id) {
             return this.currentId = id;
-        }
+        },
     }
 })
 
